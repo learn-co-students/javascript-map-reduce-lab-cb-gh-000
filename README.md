6c1ef97773dc80a1106e1ea78d5b3d889fd30814
Thomas Tuts <thomas@thomastuts.com>
2016-07-17 21:25:33 +0200

    Add content and tests for exercises (#1)
    * Add content and tests for exercises
    
    * Add test for printed HTML results

--- README.md
+++ README.md
@@ -8,23 +8,53 @@ JavaScript Map-Reduce Lab
 3. Practice using `map()` and `reduce()` together
 
 ## Introduction
+This is it, the final challenge! Now that we're confident in our ability to use `.map()` and `.reduce()`, these
+challenges should prove sufficiently easy!
 
-- We're going to work with data from the GitHub API here — specifically, issues by repository
-- So frame the narrative in that way, and give students some insight into how GitHub _really_ looks
-- Introduce students to `map()` and `reduce()` on the array prototype :P
+In this lab, we're going to work with GitHub API data — more specifically, issues related to courses on Learn.co. We'll
+analyze this data using `.map()` and `.reduce()`, providing us with (some not so) valuable insights!
 
-## Implementation
+## Updating the API
+Let's say GitHub has updated the URL to their API to a new one. That means we're using outdated data! Let's bring our
+data up-to-date.
 
-- First practice on chunks of the data:
-  - Get the sum of an array of integers using `reduce()`
-  - Transform data using `map()`
-- Then work with the real data
-  - Break the problem down so that students can clearly see individual steps
-  - But be sure that there's some challenge for the students as well
-- I've set up issues.js, and index.html for your use (as well as the test file)
-- Students should have to display the data in a table
-  - Issue totals
-  - Maybe word counts as a stretch goal (using issue.body)
+* Create a variable called `issuesWithUpdatedApiUrl`
+* The value of this variable is the mapped `issues` array, replacing any mention of `'api.github.com'` (in `issue.url`)
+with `'api-v2.github.com'`
+* Make sure you don't modify the _original_ objects in the `issues` array
+
+## Counting the comments
+To get a quick overview of our community's activity, let's sum up all comments of every issue.
+
+* The result should be in a variable called `commentCountAcrossIssues`
+* First, map the `issues` array and pull out the `comments_count`, to make things easier
+* Next, reduce the array of comment counts and add them all together
+* Feel free to chain these operations on the `issues` array for brevity — no need for a temporary variable!
+
+## Filtering for open issues
+Looking at the data, it seems like some issues are closed already. Those aren't really relevant, so let's create a new
+array that only has _open_ issues.
+
+* The result should be in a variable called `openIssues`
+* This new array should only contain issues where `issue.state` is set to `'open'`
+
+## I'm not a robot
+Some issues have been automatically created by the Learn.co bot. Let's filter these out, so we only see issues created
+by actual humans.
+
+* The result should be in a variable called `nonAutomaticIssues`
+* This new array should only contain issues that are not automatically created
+* Hint: check the `issue.body` for a clue
+
+## Showing off
+Now that we have our `nonAutomaticIssues`, let's print these out in a table in our HTML page. The base markup has been
+provided for you in `index.html` — all we need to do is add rows to the table body.
+
+* Use the DOM API to set the HTML of the table body to a list of table rows that have three columns: the issue body, the
+date and the state of the issue (open or closed)
+* To easily create your HTML markup for all the rows, use `.map()`
+* Hint: the `innerHTML` property expects a string, so don't forget to turn your array into one big string by using
+[`.join()`](https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/join)
 
 ## Resources
 
--- test/index-test.js
+++ test/index-test.js
@@ -1,18 +1,87 @@
-const expect = require('expect')
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
+const expect = require('expect');
 
-describe('index', () => {
-  const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
-  const src = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+const fs = require('fs');
+const cheerio = require('cheerio');
+const jsdom = require('mocha-jsdom');
+const path = require('path');
+const babel = require('babel-core');
 
+const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf8');
+
+const babelResult = babel.transformFileSync(path.resolve(__dirname, '..', 'index.js'), {
+  presets: ['es2015']
+});
+
+describe('Map/reduce lab', () => {
   jsdom({
     html,
-    src
-  })
+    src: babelResult.code
+  });
 
-  it('runs', () => {
-    expect(true).to.be.true
-  })
-})
+  describe('Updating the API', function () {
+    it('should have an `issuesWithUpdatedApiUrl` array', function () {
+      expect(issuesWithUpdatedApiUrl).toBeAn('array');
+    });
+
+    it('should have the same length as the original `issues` array', function () {
+      expect(issuesWithUpdatedApiUrl.length).toEqual(issues.length);
+    });
+
+    it('should have replaced the API URL in all issues', function () {
+      const hasPassed = issuesWithUpdatedApiUrl.every(issue => {
+        const hasOldUrl = issue.url.includes('api.github.com');
+        const hasNewUrl = issue.url.includes('api-v2.github.com');
+        return !hasOldUrl && hasNewUrl;
+      });
+      expect(hasPassed).toBeTruthy('The API URL has not been replaced properly in every issue.');
+    });
+
+    it('should not have modified the original objects in the `issues` array', function () {
+      const hasPassed = issues.every(issue => {
+        const hasOldUrl = issue.url.includes('api.github.com');
+        return hasOldUrl;
+      });
+      expect(hasPassed).toBeTruthy('The original objects in the `issues` array have been modified.');
+    });
+  });
+
+  describe('Counting the comments', function () {
+    it('should have a `commentCountAcrossIssues` variable', function () {
+      expect(commentCountAcrossIssues).toBeA('number');
+    });
+
+    it('should have the right amount of comments', function () {
+      expect(commentCountAcrossIssues).toEqual(703);
+    });
+  });
+
+  describe('Filtering for open arrays', function () {
+    it('should have an `openIssues` array', function () {
+      expect(openIssues).toBeAn('array');
+    });
+
+    it('should only contain open issues', function () {
+      const hasPassed = openIssues.every(issue => issue.state === 'open');
+      expect(hasPassed).toBeTruthy('The `openIssues` array contains issues that are not open.');
+    });
+  });
+
+  describe("I'm not a robot", function () {
+    it('should have a `nonAutomaticIssues` array', function () {
+      expect(nonAutomaticIssues).toBeAn('array');
+    });
+
+    it('should only contain issues that are not created automatically', function () {
+      const hasPassed = nonAutomaticIssues.every(issue => !issue.body.includes('automatically created by learn.co'));
+      expect(hasPassed).toBeTruthy('The `nonAutomaticIssues` array contains issues that were automatically created.');
+    });
+  });
+
+  describe("Showing off", function () {
+    it('should add the appropriate amount of table rows to the HTML', function () {
+      const $ = cheerio.load(document.body.innerHTML);
+      const $rows = $('#results > tr');
+      expect($rows.length).toEqual(nonAutomaticIssues.length);
+    });
+  });
+});
