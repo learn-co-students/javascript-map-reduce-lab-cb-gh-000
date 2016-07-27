e3ea86f22374577adf95e3cb87d66a635a18f8c6
Charles Pletcher <cpletcher2@gmail.com>
2016-07-27 11:28:22 -0400

    Add jQuery :(

--- test/index-test.js
+++ test/index-test.js
@@ -60,7 +60,7 @@ describe('Map/reduce lab', () => {
 
   describe("Showing off", function () {
     it('should add the appropriate amount of table rows to the HTML', function () {
-      const $ = typeof cheerio !== 'undefined' ? cheerio.load(document.body.innerHTML) : document.querySelectorAll;
+      const $ = typeof cheerio !== 'undefined' ? cheerio.load(document.body.innerHTML) : jQuery;
       const $rows = $('#results > tr');
       expect($rows.length).toEqual(nonAutomaticIssues.length);
     });
