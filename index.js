eb098cb0f71fc378c10a8c683e42f58a555566f6
Dominik Kurbiel <github email address>
2017-07-30 01:23:05 +0200

    Done.

--- index.js
+++ index.js
@@ -9000,3 +9000,41 @@ const issues = [
     "url": "https://api.github.com/repos/learn-co-curriculum/js-donut-lab/issues/2"
   }
 ];
+
+
+const issuesWithUpdatedApiUrl = issues.map(function(issue) {
+	var issueUrl = issue.url.replace("api.github.com", "api-v2.github.com");
+	return Object.assign({}, issue, {url: issue.url.replace("api.github.com", "api-v2.github.com")});
+});
+
+const commentCountAcrossIssues = issues.map(function(issue) {
+		return issue.comments_count;
+	
+}).reduce(function(prev, curr) {
+	return prev + curr;
+},0);
+
+
+const openIssues = issues.filter(function(issue) {
+	
+	if (issue.state === 'open') {
+		return issue
+	}
+});
+
+
+const nonAutomaticIssues = issues.filter(function(issue) {
+	if(issue.body != 'This pull request has been automatically created by learn.co.') {
+		return issue
+	}
+});
+
+var table = nonAutomaticIssues.map(function(issue) {
+	
+	return "<tr><th>${issue.body}</th><th>${issue.created_at}</th><th>${issue.state}</th></tr>"
+	
+	
+})
+
+document.querySelector('#results').innerHTML= table.join();
+
